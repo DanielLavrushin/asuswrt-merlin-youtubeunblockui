98d3678af0c46beb069029506984c007f251940f
Daniel Lavrushin <dala@milestone.dk>
2025-04-09 16:02:04 +0200

    Enhance update function: Add version-specific download and extraction process

--- src/backend/update.sh
+++ src/backend/update.sh
@@ -10,18 +10,40 @@ update() {
 
     local url="https://github.com/daniellavrushin/asuswrt-merlin-youtubeunblockui/releases/latest/download/asuswrt-merlin-yuui.tar.gz"
     if [ ! $specific_version = "latest" ]; then
-        local url=$(github_proxy_url "https://github.com/daniellavrushin/asuswrt-merlin-youtubeunblockui/releases/download/v$specific_version/asuswrt-merlin-yuui.tar.gz")
+        local url="https://github.com/daniellavrushin/asuswrt-merlin-youtubeunblockui/releases/download/v$specific_version/asuswrt-merlin-yuui.tar.gz"
     fi
 
     local temp_file="/tmp/asuswrt-merlin-yuui.tar.gz"
+    local jffs_addons_path="/jffs/addons"
 
-    printlog true "Downloading the latest version..."
-    update_loading_progress "Downloading the latest version..."
+    printlog true "Downloading the $specific_version version..."
+    update_loading_progress "Downloading the $specific_version version..."
 
     if wget -q --show-progress -O "$temp_file" "$url"; then
         printlog true "Download completed successfully."
     else
-        printlog true "Failed to download the latest version. Exiting."
+        printlog true "Failed to download the $specific_version version. Exiting." $CERR
+        return 1
+    fi
+
+    printlog true "Cleaning up existing installation..."
+    if rm -rf "$ADDON_JFFS_ADN_DIR"; then
+        printlog true "Old installation removed."
+    else
+        printlog true "Failed to remove the old installation. Exiting." $CERR
+        return 1
+    fi
+
+    printlog true "Extracting the package..."
+    update_loading_progress "Extracting the package..."
+    if tar -xzf "$temp_file" -C "$jffs_addons_path"; then
+        printlog true "Extraction completed."
+
+        mv /jffs/addons/yuui/yuui "$ADDON_SCRIPT"
+        chmod 0777 "$ADDON_SCRIPT"
+
+    else
+        printlog true "Failed to extract the package. Exiting." $CERR
         return 1
     fi
 
